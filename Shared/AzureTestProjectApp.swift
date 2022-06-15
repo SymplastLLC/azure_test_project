5a96757e4a067b03f5699d29d51a2e9ad58428f5
kvyatkovskys <sergejkvyatkovskij@gmail.com>
2022-06-15 10:33:29 +0300

    Initial Commit

---
+++ Shared/AzureTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AzureTestProjectApp.swift
+//  Shared
+//
+//  Created by Sergei Kviatkovskii on 6/15/22.
+//
+
+import SwiftUI
+
+@main
+struct AzureTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
